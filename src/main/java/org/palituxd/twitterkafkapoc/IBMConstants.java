bfb6211a27fce1bbd81661ad58d77e8c75e5cdb9
PalituxD <palituxd@gmail.com>
2019-05-27 10:38:22 -0500

    Update IBMConstants.java

--- src/main/java/org/palituxd/twitterkafkapoc/IBMConstants.java
+++ src/main/java/org/palituxd/twitterkafkapoc/IBMConstants.java
@@ -2,7 +2,7 @@ package org.palituxd.twitterkafkapoc;
 
 public interface IBMConstants {
 
-    String KEY = "OEKILy5-BDb0-WBVciD-4K-dD2Sjfh4J_iJEAR_48TLk";
-    String URL = "https://gateway.watsonplatform.net/natural-language-understanding/api";
+    String KEY = "********";
+    String URL = "********";
 
 }
