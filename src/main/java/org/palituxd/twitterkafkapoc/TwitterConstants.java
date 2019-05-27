184a0b8020e459bc72b0c83bc7fba52b972db65e
PalituxD <palituxd@gmail.com>
2019-05-27 10:38:37 -0500

    Update TwitterConstants.java

--- src/main/java/org/palituxd/twitterkafkapoc/TwitterConstants.java
+++ src/main/java/org/palituxd/twitterkafkapoc/TwitterConstants.java
@@ -1,9 +1,9 @@
 package org.palituxd.twitterkafkapoc;
 
 public interface TwitterConstants {
-    String CONSUMER_KEY = "NGhk2aRJ5xjizrbqtGIeye0w3";
-    String CONSUMER_SECRET = "WuxlNY7URoAv1QUp3HjeUUeGJXzDR4Pmn9ZUYOPgm7OutANbaW";
-    String ACCESS_TOKEN = "1119299819460284416-8Qxsqm5e3Ki9ZweROWzkXgqPsRBY5S";
-    String TOKEN_SECRET = "OVkWDjEv6dQnJ7oE76fiVOpJV7DVreaIKoxREFKjnZV6O";
+    String CONSUMER_KEY = "********";
+    String CONSUMER_SECRET = "********";
+    String ACCESS_TOKEN = "********";
+    String TOKEN_SECRET = "********";
     String HASHTAG = "Peru";
 }
