d9276c8f949dbc677c1a79200183718bca4b20b3
PalituxD <palituxd@gmail.com>
2019-05-27 10:38:59 -0500

    Update BDConstants.java

--- src/main/java/org/palituxd/twitterkafkapoc/BDConstants.java
+++ src/main/java/org/palituxd/twitterkafkapoc/BDConstants.java
@@ -1,7 +1,7 @@
 package org.palituxd.twitterkafkapoc;
 
 public interface BDConstants {
-    String URL_CONNECTION = "mongodb+srv://root:k95rTKDFQjUJlI4G@cluster0-4lizd.mongodb.net/test?retryWrites=true";
+    String URL_CONNECTION = "mongodb+srv://root:************@cluster0-4lizd.mongodb.net/test?retryWrites=true";
     String DATABASE = "Twitter";
     String DOCUMENT = "Tweet2";
 }
